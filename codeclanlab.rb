e6e75c55c95334bf7369487c81e463a96b7697c0
Ross Hill <rosshill88@gmail.com>
2017-11-27 16:17:35 +0000

    created getters

---
+++ codeclanlab.rb
@@ -0,0 +1,18 @@
+class Student
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+
+  end
+
+  def get_name
+
+  end
+
+  def get_cohort
+
+  end
+
+
+end
---
+++ specs/codeclanlab_specs.rb
@@ -0,0 +1,13 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../codeclanlan.rb')
+
+class Student < MiniTest::MiniTest
+
+  def setup
+    
+
+
+
+
+end
