99137f38a5e0d4eda375ece7950f4fe1749f083e
Ross Hill <rosshill88@gmail.com>
2017-11-27 20:37:45 +0000

    Refactor and add array tested and working

--- codeclanlab.rb
+++ codeclanlab.rb
@@ -1,43 +1,64 @@
-class Student
-
-  def initialize(name, cohort, talk, language)
-    @name = name
-    @cohort = cohort
-    @talk = talk
-    @language = language
-  end
-
-  def name
-    return @name
-  end
-
-  def cohort
-    return @cohort
-  end
-
-  def update_name(name)
-    @name = name
-  end
-
-  def update_cohort(cohort)
-    @cohort = cohort
-  end
-
-  def talk
-    return @talk
-  end
-
-  def say_favourite_language(language)
-    return "I love #{language}"
-  end
-end
-
-# class Team
-#   def initialize(team_name, players=[], )
+# class Student
 #
+#   def initialize(name, cohort, talk, language)
+#     @name = name
+#     @cohort = cohort
+#     @talk = talk
+#     @language = language
 #   end
+#
+#   def name
+#     return @name
+#   end
+#
+#   def cohort
+#     return @cohort
+#   end
+#
+#   def update_name(name)
+#     @name = name
+#   end
+#
+#   def update_cohort(cohort)
+#     @cohort = cohort
+#   end
+#
+#   def talk
+#     return @talk
+#   end
+#
+#   def say_favourite_language(language)
+#     return "I love #{language}"
+#   end
+# end
 
+class Team
 
+  attr_reader :team_name, :players, :coach
 
+  def initialize(team_name, players=[], coach)
+    @team_name = team_name
+    @players = []
+    @coach = coach
+  end
 
+  # def team_name
+  #   return @team_name
+  # end
+  #
+  # def players
+  #   return @players
+  # end
+  #
+  # def coach
+  #   return @coach
+  # end
+  #
+  def update_coach_name(name)
+    @coach = name
+  end
+
+  def add_player(name)
+    @players = [] << name
+  end
 end
--- specs/codeclanlab_specs.rb
+++ specs/codeclanlab_specs.rb
@@ -2,38 +2,65 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../codeclanlab.rb')
 
-class TestStudent < MiniTest::Test
+class TestTeam < MiniTest::Test
 
-def test_name
-    codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-    assert_equal("Ross", codeclan.name)
+# def test_name
+#     codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#     assert_equal("Ross", codeclan.name)
+# end
+#
+# def test_cohort
+#   codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#   assert_equal(18, codeclan.cohort)
+# end
+#
+# def test_update_name
+#   codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#   codeclan.update_name("Manny")
+#   assert_equal("Manny", codeclan.name)
+# end
+#
+# def test_update_cohort
+#   codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#   codeclan.update_cohort(17)
+#   assert_equal(17, codeclan.cohort)
+# end
+#
+# def test_talk
+#   codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#   assert_equal("I can talk", codeclan.talk)
+# end
+#
+# def test_say_favourite_language
+#   codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+#   assert_equal("I love Ruby", codeclan.say_favourite_language("Ruby"))
+# end
+
+def test_team_name
+  team = Team.new("Ross FC", [], "Manny")
+  assert_equal("Ross FC", team.team_name)
 end
 
-def test_cohort
-  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-  assert_equal(18, codeclan.cohort)
+def test_players
+  team = Team.new("Ross FC", [], "Manny")
+  assert_equal([], team.players)
 end
 
-def test_update_name
-  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-  codeclan.update_name("Manny")
-  assert_equal("Manny", codeclan.name)
+def test_coach
+  team = Team.new("Ross FC", [], "Manny")
+  assert_equal("Manny", team.coach)
 end
 
-def test_update_cohort
-  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-  codeclan.update_cohort(17)
-  assert_equal(17, codeclan.cohort)
+def test_update_coach_name
+  team = Team.new("Ross FC", [], "Manny")
+  team.update_coach_name("Gordon Strachan")
+  assert_equal("Gordon Strachan", team.coach)
 end
 
-def test_talk
-  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-  assert_equal("I can talk", codeclan.talk)
-end
-
-def test_say_favourite_language
-  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
-  assert_equal("I love Ruby", codeclan.say_favourite_language("Ruby"))
+def test_add_player
+  team = Team.new("Ross FC", [], "Manny")
+  team.add_player("Ryan Giggs")
+  assert_equal("Ryan Giggs", team.players[0])
 end
 
 
