d41fde1858e412f32ccdee4bae635ca6f23838ee
Ross Hill <rosshill88@gmail.com>
2017-11-27 17:27:22 +0000

    Part A complete

--- specs/codeclanlab_specs.rb
+++ specs/codeclanlab_specs.rb
@@ -1,11 +1,40 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('../codeclanlan.rb')
+require_relative('../codeclanlab.rb')
 
-class Student < MiniTest::MiniTest
+class TestStudent < MiniTest::Test
 
-  def setup
-    
+def test_name
+    codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+    assert_equal("Ross", codeclan.name)
+end
+
+def test_cohort
+  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+  assert_equal(18, codeclan.cohort)
+end
+
+def test_update_name
+  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+  codeclan.update_name("Manny")
+  assert_equal("Manny", codeclan.name)
+end
+
+def test_update_cohort
+  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+  codeclan.update_cohort(17)
+  assert_equal(17, codeclan.cohort)
+end
+
+def test_talk
+  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+  assert_equal("I can talk", codeclan.talk)
+end
+
+def test_say_favourite_language
+  codeclan = Student.new("Ross", 18, "I can talk", "Ruby")
+  assert_equal("I love Ruby", codeclan.say_favourite_language("Ruby"))
+end
 
 
 
