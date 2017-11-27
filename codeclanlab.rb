5f4f363148d9dcb2bfb5b08500b94275e6d3ab33
Ross Hill <rosshill88@gmail.com>
2017-11-27 17:36:04 +0000

    Corrected save file

--- codeclanlab.rb
+++ codeclanlab.rb
@@ -1,18 +1,43 @@
 class Student
 
-  def initialize(name, cohort)
+  def initialize(name, cohort, talk, language)
     @name = name
     @cohort = cohort
-
+    @talk = talk
+    @language = language
   end
 
-  def get_name
-
+  def name
+    return @name
   end
 
-  def get_cohort
-
+  def cohort
+    return @cohort
   end
+
+  def update_name(name)
+    @name = name
+  end
+
+  def update_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def talk
+    return @talk
+  end
+
+  def say_favourite_language(language)
+    return "I love #{language}"
+  end
+end
+
+# class Team
+#   def initialize(team_name, players=[], )
+#
+#   end
+
+
 
 
 end
